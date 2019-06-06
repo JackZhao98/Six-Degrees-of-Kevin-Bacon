bcc422c589629dc969844ddee1c6379412de86d9
Jack Zhao <jack_zhao98@outlook.com>
2019-06-06 00:49:51 -0700

    rename

--- Six Degrees of Kevin Bacon/main.cpp
+++ Six Degrees of Kevin Bacon/main.cpp
@@ -14,16 +14,20 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     IMDb i;
     vector<string> films,casts;
+    auto start = chrono::high_resolution_clock::now();
     i.loadDataBase();
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = end - start;
+    cout << chrono::duration_cast<chrono::seconds>(duration).count();
     
     i.getCredits("Kevin Bacon", films);
     i.getCast("Star Wars: Shadows of the Empire", casts);
-    for (int i = 0; i < films.size(); ++ i) {
-        cout << films[i] << "\n";
-    }
-    for (int i = 0; i < casts.size(); ++ i) {
-        cout << casts[i] << "\n";
-    }
+//    for (int i = 0; i < films.size(); ++ i) {
+//        cout << films[i] << "\n";
+//    }
+//    for (int i = 0; i < casts.size(); ++ i) {
+//        cout << casts[i] << "\n";
+//    }
     return 0;
 }
 
